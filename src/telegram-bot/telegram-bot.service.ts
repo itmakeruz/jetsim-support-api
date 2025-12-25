445c01eedf2a40bb8bac372a03a0f80b53618a76
Tohir Ravshanov <rth@MacBook-Pro-Tohir.local>
2025-12-25 12:29:18 +0500

    fx

--- src/telegram-bot/telegram-bot.service.ts
+++ src/telegram-bot/telegram-bot.service.ts
@@ -52,17 +52,17 @@ export class TelegramBotService {
         where: { chat_id: chat_id.toString() },
       });
 
-      if (!user) {
-        let newUser = await this.prisma.users.create({
-          data: {
-            chat_id: chat_id.toString(),
-            phone_number: phone_number,
-            lang: 'ru',
-            is_online: true,
-          },
-        });
-        user = newUser;
-      }
+      // if (!user) {
+      //   let newUser = await this.prisma.users.create({
+      //     data: {
+      //       chat_id: chat_id.toString(),
+      //       phone_number: phone_number,
+      //       lang: 'ru',
+      //       is_online: true,
+      //     },
+      //   });
+      //   user = newUser;
+      // }
 
       let action: actionModel = Object(user?.action);
       action.step = 'ticket';
