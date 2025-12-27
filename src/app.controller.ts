0b9b9305638d177945dc897113283d2f8e1dbe29
Tohir Ravshanov <rth@MacBook-Pro-Tohir.local>
2025-12-27 14:58:50 +0500

    fx

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -119,6 +119,8 @@ export class renderController {
     if (file?.size > 100000000) {
       throw new BadRequestException(file_size_error[lang]);
     }
+    console.log(user);
+
     return await this.service.upload(file, body, user, lang);
   }
 }
